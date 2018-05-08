4efcf43af366ce9090a2937a93101ecdd87b6c23
Zach Jones <zachtjones16@gmail.com>
2018-05-08 10:55:25 -0400

    Updated test script to remove compiled IL when done

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+rm differences.txt
+
 java -cp "../" JavaCompiler EnumTest.java
-diff EnumTest.jil expected/EnumTest.jil > differences.txt
+diff EnumTest.jil expected/EnumTest.jil >> differences.txt
 if [ `echo $?` -eq "1" ] ; then
 	echo "EnumTest.java failed"
 fi
+rm -f EnumTest.jil
+
 
 
