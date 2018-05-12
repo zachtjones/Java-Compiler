a512d06127fcc64a4a4f90cf4ae34a2372b32908
Zach Jones <zachtjones16@gmail.com>
2018-05-12 11:49:20 -0400

    Updated the test for enums

--- tests/makeScript.sh
+++ tests/makeScript.sh
@@ -10,8 +10,8 @@ for file in *.java; do
 	jilName=${file/.java/.jil} # replace .java with .jil
 	echo "diff $jilName expected/$jilName >> differences.txt" >> runTests.sh
 	echo "if [ \`echo \$?\` -eq \"1\" ] ; then" >> runTests.sh
-	echo "	echo \"$file\ failed!\"" >> runTests.sh
-	echo "	numFailing=\$numFailing+1" >> runTests.sh
+	echo "	echo \"$file failed!\"" >> runTests.sh
+	echo "	let numFailing=\$numFailing+1" >> runTests.sh
 	echo "fi" >> runTests.sh
 	echo "rm -f $jilName" >> runTests.sh
 	total=$(($total + 1))
@@ -19,7 +19,7 @@ done
 
 echo "numTotal=$total" >> runTests.sh
 
-echo "passed=\$((\$numTotal - \$numFailing))" >> runTests.sh
+echo "let passed=\$numTotal-\$numFailing" >> runTests.sh
 echo "echo \"\$passed / \$numTotal tests passed\"" >> runTests.sh
 
 chmod +x runTests.sh
--- tests/runTests.sh
+++ tests/runTests.sh
@@ -3,10 +3,10 @@ numFailing=0
 java -cp "../" JavaCompiler EnumTest.java
 diff EnumTest.jil expected/EnumTest.jil >> differences.txt
 if [ `echo $?` -eq "1" ] ; then
-	echo "EnumTest.java\ failed!"
-	numFailing=$numFailing+1
+	echo "EnumTest.java failed!"
+	let numFailing=$numFailing+1
 fi
 rm -f EnumTest.jil
 numTotal=1
-passed=$(($numTotal - $numFailing))
+let passed=$numTotal-$numFailing
 echo "$passed / $numTotal tests passed"
