378eadbe148a7d1a572e72708295b6a2183f0e51
Zach Jones <zachtjones16@gmail.com>
2018-05-25 18:34:32 -0400

    Represents expression = expression or expression += expression or ...

---
+++ tree/AssignmentNode.java
@@ -0,0 +1,21 @@
+package tree;
+
+/** left type right */
+public class AssignmentNode implements ExpressionNode {
+	public final static int ASSIGN = 0;
+	public final static int STARASSIGN = 1;
+	public final static int SLASHASSIGN = 2;
+	public final static int REMASSIGN = 3;
+	public final static int PLUSASSIGN = 4;
+	public final static int MINUSASSIGN = 5;
+	public final static int LSHIFTASSIGN = 6;
+	public final static int RSIGNEDSHIFTASSIGN = 7; // >>=
+	public final static int RUNSIGNEDSHIFTASSIGN = 8; // >>>=
+	public final static int ANDASSIGN = 9;
+	public final static int XORASSIGN = 10;
+	public final static int ORASSIGN = 11;
+
+	public ExpressionNode left;
+	public int type;
+	public ExpressionNode right;
+}
