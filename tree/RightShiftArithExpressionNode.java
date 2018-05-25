2a2079a0b202be46863a3bb0fb7e42651b043fd1
Zach Jones <zachtjones16@gmail.com>
2018-05-25 18:12:14 -0400

    Represents right shift expressions

---
+++ tree/RightShiftArithExpressionNode.java
@@ -0,0 +1,7 @@
+package tree;
+
+/** left >> right (sign filled)*/
+public class RightShiftArithExpressionNode implements ExpressionNode {
+    public ExpressionNode left;
+    public ExpressionNode right;
+}
---
+++ tree/RightShiftLogExpressionNode.java
@@ -0,0 +1,7 @@
+package tree;
+
+/** left >>> right (0 filled) */
+public class RightShiftLogExpressionNode implements ExpressionNode {
+    public ExpressionNode left;
+    public ExpressionNode right;
+}
