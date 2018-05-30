f0105880fc7fb53ba087bfc665a6d9c9b174268c
Zach Jones <zachtjones16@gmail.com>
2018-05-30 17:21:41 -0400

    Handles literal expressions to IL

--- tree/LiteralExpressionNode.java
+++ tree/LiteralExpressionNode.java
@@ -1,5 +1,26 @@
 package tree;
 
+import java.io.IOException;
+
+import helper.ClassLookup;
+import helper.CompileException;
+import intermediate.InterFunction;
+import intermediate.LoadLiteralStatement;
+import intermediate.RegisterAllocator;
+
 public class LiteralExpressionNode implements Expression {
+	
     public String value;
+
+	@Override
+	public void resolveImports(ClassLookup c) throws IOException {
+		// nothing to do
+	}
+
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r) throws CompileException {
+		// the IL code does the work here
+		int number = r.getNextLabel();
+		f.statements.add(new LoadLiteralStatement(value, number));	
+	}
 }
