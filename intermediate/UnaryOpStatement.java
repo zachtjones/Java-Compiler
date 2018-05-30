35feca85897b014cbd9e33d62f29792f68cb3740
Zach Jones <zachtjones16@gmail.com>
2018-05-30 17:03:50 -0400

    Represents instructions that are Register = OP Register

---
+++ intermediate/UnaryOpStatement.java
@@ -0,0 +1,22 @@
+package intermediate;
+
+/** dest = OP src2 */
+public class UnaryOpStatement implements InterStatement {
+	public static final char BITNOT = '~';
+	
+	Register src1;
+	Register dest;
+	char type;
+	
+	/** Creates an unary operation statement */
+	public UnaryOpStatement(Register src1, Register dest, char type) {
+		this.src1 = src1;
+		this.dest = dest;
+		this.type = type;
+	}
+	
+	@Override
+	public String toString() {
+		return dest.toString() + " = " + type + " " + src1.toString() + ";";
+	}
+}
