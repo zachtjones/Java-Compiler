cbee8ae5e9bf917a3c39e8e81362807c23d8d3cc
Zach Jones <zachtjones16@gmail.com>
2018-05-30 22:54:13 -0400

    Added compiling shift operations

--- tree/RightShiftLogExpressionNode.java
+++ tree/RightShiftLogExpressionNode.java
@@ -1,7 +1,37 @@
 package tree;
 
+import java.io.IOException;
+
+import helper.ClassLookup;
+import helper.CompileException;
+import intermediate.BinaryOpStatement;
+import intermediate.InterFunction;
+import intermediate.Register;
+import intermediate.RegisterAllocator;
+
 /** left >>> right (0 filled) */
 public class RightShiftLogExpressionNode implements Expression {
     public Expression left;
     public Expression right;
+    
+	@Override
+	public void resolveImports(ClassLookup c) throws IOException {
+		left.resolveImports(c);
+		right.resolveImports(c);
+	}
+	
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r) throws CompileException {
+		left.compile(s, f, r);
+		Register leftResult = r.getLast();
+		
+		right.compile(s, f, r);
+		Register rightResult = r.getLast();
+		
+		// result is of type left
+		Register result = r.getNext(leftResult.type);
+		f.statements.add(new BinaryOpStatement(
+				leftResult, rightResult, result, 
+				(char)BinaryOpStatement.RSHIFTLOG));
+	}
 }
