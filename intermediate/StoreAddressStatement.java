9fb6691c6f759d0bea374fea5e48342567f9757b
Zach Jones <zachtjones16@gmail.com>
2018-05-31 15:16:31 -0400

    Used to store a register's contents into the address of another

---
+++ intermediate/StoreAddressStatement.java
@@ -0,0 +1,17 @@
+package intermediate;
+
+/** store %src at %addr */
+public class StoreAddressStatement implements InterStatement {
+	Register src;
+	Register addr;
+	
+	public StoreAddressStatement(Register src, Register addr) {
+		this.src = src;
+		this.addr = addr;
+	}
+	
+	@Override
+	public String toString() {
+		return "put " + src.toString() + " at " + addr.toString() + ";";
+	}
+}
