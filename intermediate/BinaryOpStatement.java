eca137a13690e12ba6947698e6e4a1455beae3dd
Zach Jones <zachtjones16@gmail.com>
2018-05-30 22:53:50 -0400

    Added compiling subtraction

--- intermediate/BinaryOpStatement.java
+++ intermediate/BinaryOpStatement.java
@@ -3,15 +3,19 @@ package intermediate;
 /** dest = src1 OP src2 */
 public class BinaryOpStatement implements InterStatement {
 	public static final char ADD = '+';
+	public static final char SUB = '-';
 	public static final char AND = '&';
 	public static final char OR = '|';
 	public static final char XOR = '^';
 	public static final char DIV = '/';
+	public static final int LSHIFT = 0;
+	public static final int RSHIFTARITH = 1; // sign-filled
+	public static final int RSHIFTLOG = 2; // 0 filled.
 	
 	Register src1;
 	Register src2;
 	Register dest;
-	char type;
+	int type;
 	
 	/** Creates an addition statement */
 	public BinaryOpStatement(Register src1, Register src2, Register dest, char type) {
@@ -23,6 +27,17 @@ public class BinaryOpStatement implements InterStatement {
 	
 	@Override
 	public String toString() {
-		return dest.toString() + " = " + src1.toString() + " " + type + " " + src2.toString() + ";";
+		if (type < 10) {
+			if (type == LSHIFT) {
+				return dest.toString() + " = " + src1.toString() + " << " + src2.toString() + ";";
+			} else if (type == RSHIFTARITH) {
+				return dest.toString() + " = " + src1.toString() + " >> " + src2.toString() + ";";
+			} else {
+				return dest.toString() + " = " + src1.toString() + " >>> " + src2.toString() + ";";
+			}
+		} else { // single char representation
+			return dest.toString() + " = " + src1.toString() + " " + type + " " + src2.toString() + ";";
+
+		}
 	}
 }
--- tree/SubtractExpressionNode.java
+++ tree/SubtractExpressionNode.java
@@ -1,7 +1,33 @@
 package tree;
 
+import java.io.IOException;
+
+import helper.ClassLookup;
+import helper.CompileException;
+import intermediate.BinaryOpStatement;
+import intermediate.InterFunction;
+import intermediate.Register;
+import intermediate.RegisterAllocator;
+
 /** left - right */
 public class SubtractExpressionNode implements Expression {
     public Expression left;
     public Expression right;
+    
+	@Override
+	public void resolveImports(ClassLookup c) throws IOException {
+		left.resolveImports(c);
+		right.resolveImports(c);
+	}
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r) throws CompileException {
+		left.compile(s, f, r);
+		Register rightResult = r.getLast();
+		
+		right.compile(s, f, r);
+		Register leftResult = r.getLast();
+		
+		Register result = r.getNext(Register.getLarger(leftResult.type, rightResult.type));
+		f.statements.add(new BinaryOpStatement(leftResult, rightResult, result, '-'));
+	}
 }
