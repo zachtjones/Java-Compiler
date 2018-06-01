f57f97370bd35ca1d18fd8da46e8ee87fa892be9
Zach Jones <zachtjones16@gmail.com>
2018-05-31 21:47:31 -0400

    Added overloaded method for getting next by String type

--- intermediate/RegisterAllocator.java
+++ intermediate/RegisterAllocator.java
@@ -10,6 +10,24 @@ public class RegisterAllocator {
 		return curr;
 	}
 	
+	/**
+	 * Gets the next register (of the type specified)
+	 * @param type A String that is the intermediate file's representation.
+	 */
+	public Register getNext(String type) {
+		switch(type) {
+		case "bool": return getNext(Register.BYTE);
+		case "char": return getNext(Register.SHORT);
+		case "byte": return getNext(Register.BYTE);
+		case "short": return getNext(Register.SHORT);
+		case "int": return getNext(Register.INT);
+		case "long": return getNext(Register.LONG);
+		case "float": return getNext(Register.FLOAT);
+		case "double": return getNext(Register.DOUBLE);
+		}
+		return getNext(Register.REFERENCE);
+	}
+	
 	/** Makes the current register a new label one, and returns it's number. */
 	public int getNextLabel() {
 		curr = new Register(curr.num + 1, Register.LABEL);
@@ -20,4 +38,5 @@ public class RegisterAllocator {
 	public Register getLast() {
 		return curr;
 	}
+
 }
