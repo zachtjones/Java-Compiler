77a81f130b517b3fb2b124e99b5a0d485fa3917e
Zach Jones <zachtjones16@gmail.com>
2018-05-31 23:52:04 -0400

    Added test for a class with instance and static field, and getter/setteers

---
+++ tests/BasicClass.java
@@ -0,0 +1,22 @@
+// this file is for testing a static and instance field, as well as
+//  minimal method example
+public class BasicClass {
+	public static int a = 1;
+	public int b = 2;
+
+	public void setB(int c) {
+		b = c;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public static void setA(int c) {
+		a = c;
+	}
+
+	public static void getA() {
+		return a;
+	}
+}
