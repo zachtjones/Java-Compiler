0714e9c464108d5f1b41ffafd3222737ca1c8ab8
Zach Jones <zachtjones16@gmail.com>
2018-06-02 17:14:00 -0400

    Added an object to store some past info

---
+++ tree/CompileHistory.java
@@ -0,0 +1,10 @@
+package tree;
+
+/**
+ * This class is used to hold a small history of the current compilation process.
+ * For example, this stores if the last part compiled was "this", or "this.x",
+ * so if the next Node is (a+b, c), the compiler knows what to do.
+ */
+public class CompileHistory {
+
+}
