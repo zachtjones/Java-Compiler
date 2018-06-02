9fc18f16936caa28d4c168237d49638f422e1011
Zach Jones <zachtjones16@gmail.com>
2018-06-02 17:58:16 -0400

    Added in compiling of this.FIELD

--- tree/PrimaryExpressionNode.java
+++ tree/PrimaryExpressionNode.java
@@ -1,8 +1,48 @@
 package tree;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class PrimaryExpressionNode implements Expression {
+import helper.ClassLookup;
+import helper.CompileException;
+import intermediate.InterFunction;
+import intermediate.RegisterAllocator;
+
+public class PrimaryExpressionNode implements Expression, LValue {
     public Expression prefix;
     public ArrayList<Expression> suffixes = new ArrayList<Expression>();
+    
+	@Override
+	public void resolveImports(ClassLookup c) throws IOException {
+		prefix.resolveImports(c);
+		for (Expression e : suffixes) {
+			e.resolveImports(c);
+		}
+	}
+	
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r, CompileHistory c) throws CompileException {
+		prefix.compile(s, f, r, c);
+		for (Expression e : suffixes) {
+			e.compile(s, f, r, c);
+		}
+		
+	}
+
+	@Override
+	public void compileAddress(SymbolTable s, InterFunction f, RegisterAllocator r, CompileHistory c) throws CompileException {
+		// compile the address of the last part
+		prefix.compile(s, f, r, c);
+		for (int i = 0; i < suffixes.size(); i++) {
+			if (i == suffixes.size() - 1) { // last one
+				if (suffixes.get(i) instanceof LValue) {
+					( (LValue) suffixes.get(i) ).compileAddress(s, f, r, c);
+				} else {
+					throw new CompileException("left side of = expression not able to assign to. " + suffixes.get(i));
+				}
+			} else {
+				suffixes.get(i).compile(s, f, r, c);
+			}
+		}
+	}
 }
--- tree/ThisExpressionNode.java
+++ tree/ThisExpressionNode.java
@@ -1,6 +1,23 @@
 package tree;
 
+import java.io.IOException;
+
+import helper.ClassLookup;
+import helper.CompileException;
+import intermediate.InterFunction;
+import intermediate.RegisterAllocator;
+
 /** "this" */
 public class ThisExpressionNode implements Expression {
+
+	@Override
+	public void resolveImports(ClassLookup c) throws IOException {
+		// nothing needed
+	}
+
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r, CompileHistory c) throws CompileException {
+		c.setThis();
+	}
 
 }
