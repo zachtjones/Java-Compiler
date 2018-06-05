bd5c9d447049ac184899a56e2fe467a78563585d
Zach Jones <zachtjones16@gmail.com>
2018-06-04 21:14:12 -0400

    Added method to get register 2 back

--- intermediate/RegisterAllocator.java
+++ intermediate/RegisterAllocator.java
@@ -2,10 +2,12 @@ package intermediate;
 
 public class RegisterAllocator {
 	private Register curr = new Register(0, 0);
+	private Register before = new Register(0, 0);
 	
 	/** Gets the next register (of the type specified)
 	 * @param type One of the constants of the Register class. */
 	public Register getNext(int type) {
+		before = curr;
 		curr = new Register(curr.num + 1, type);
 		return curr;
 	}
@@ -37,6 +39,11 @@ public class RegisterAllocator {
 	/** Gets the last number generated. */
 	public Register getLast() {
 		return curr;
+	}
+	
+	/** Gets the register before the last register generated. */
+	public Register get2Before() {
+		return before;
 	}
 
 }
