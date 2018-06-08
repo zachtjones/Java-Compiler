e2879f2af744bca0afabe62ad8979014cd29d9fd
Zach Jones <zachtjones16@gmail.com>
2018-06-08 16:41:45 -0400

    Added implementing node.Expression

--- intermediate/Register.java
+++ intermediate/Register.java
@@ -1,6 +1,21 @@
 package intermediate;
 
-public class Register {
+import java.io.IOException;
+
+import helper.ClassLookup;
+import helper.CompileException;
+import tree.CompileHistory;
+import tree.Expression;
+import tree.SymbolTable;
+
+/**
+ * Represents an abstraction of a hardware Register.
+ * This class implements Expression, as in some cases the tree needs to access
+ * statements with these directly.
+ * @author zach jones
+ *
+ */
+public class Register implements Expression {
 	
 	public static final int BYTE = 0;
 	public static final int SHORT = 1;
@@ -49,5 +64,17 @@ public class Register {
 		if (first == INT || second == INT) return INT;
 		if (first == SHORT || second == SHORT) return SHORT;
 		return BYTE;
+	}
+
+	@Override
+	public void resolveImports(ClassLookup c) throws IOException {
+		// nothing needed
+	}
+
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r, CompileHistory c) throws CompileException {
+		// make a copy statement so the result can be gotten with r.getLast()
+		Register result = r.getNext(type);
+		f.statements.add(new CopyStatement(this, result));
 	}
 }
