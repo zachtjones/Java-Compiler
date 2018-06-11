652e9d418e34265cb958da520008111d34feb37f
Zach Jones <zachtjones16@gmail.com>
2018-06-11 17:20:48 -0400

    Binary tree now, and compiles now to IL

--- tree/ConditionalOrExpressionNode.java
+++ tree/ConditionalOrExpressionNode.java
@@ -1,22 +1,43 @@
 package tree;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import helper.ClassLookup;
 import helper.CompileException;
+import intermediate.BranchStatementNEZ;
+import intermediate.ChooseStatement;
 import intermediate.InterFunction;
+import intermediate.LabelStatement;
+import intermediate.Register;
 import intermediate.RegisterAllocator;
 
 /** Chain of || of the operands */
 public class ConditionalOrExpressionNode implements Expression {
-    public ArrayList<Expression> expressions = new ArrayList<Expression>();
+	public Expression left;
+	public Expression right;
 
 	@Override
 	public void resolveImports(ClassLookup c) throws IOException {
-		for (Expression e : expressions) {
-			e.resolveImports(c);
-		}
+		left.resolveImports(c);
+		right.resolveImports(c);
+	}
+
+	@Override
+	public void compile(SymbolTable s, InterFunction f, RegisterAllocator r, CompileHistory c) throws CompileException {
+		left.compile(s, f, r, c);
+		LabelStatement end = new LabelStatement("L_" + r.getNextLabel());
+		Register leftResult = r.getLast();
+		// if left is true, jump to end
+		f.statements.add(new BranchStatementNEZ(end, leftResult));
+		
+		// compile in right half
+		right.compile(s, f, r, c);
+		Register rightResult = r.getLast();
+		
+		f.statements.add(end);
+
+		// keep in SSA, need choose statement
+		Register newReg = r.getNext(Register.BOOLEAN); // result is boolean
+		f.statements.add(new ChooseStatement(leftResult, rightResult, newReg));
 	}
 
 }
