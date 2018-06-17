da5829e26d52ff669253c1712009837ccf5146b3
Zach Jones <zachtjones16@gmail.com>
2018-06-17 16:30:17 -0400

    Updated doc comments

--- intermediate/InterStatement.java
+++ intermediate/InterStatement.java
@@ -6,6 +6,7 @@ import helper.CompileException;
 
 public interface InterStatement {
 	/** Replaces all reference types with their fully qualified name 
+	 * @param regs A mapping of the defined registers to their typeNames.
 	 * @throws CompileException If there is a type error. */
 	public void typeCheck(HashMap<Register, String> regs) throws CompileException;
 }
