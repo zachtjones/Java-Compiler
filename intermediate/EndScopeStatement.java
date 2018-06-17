28e0709d60d58f3bf06f03e12a75a3643140fc45
Zach Jones <zachtjones16@gmail.com>
2018-06-17 16:51:52 -0400

    Added type checking

--- intermediate/EndScopeStatement.java
+++ intermediate/EndScopeStatement.java
@@ -1,5 +1,9 @@
 package intermediate;
 
+import java.util.HashMap;
+
+import helper.CompileException;
+
 /** Represents the ending of a scope of a local variable. */
 public class EndScopeStatement implements InterStatement {
 	String name;
@@ -15,5 +19,10 @@ public class EndScopeStatement implements InterStatement {
 	@Override
 	public String toString() {
 		return "EndScope " + name + ";";
+	}
+
+	@Override
+	public void typeCheck(HashMap<Register, String> regs) throws CompileException {
+		// nothing needed
 	}
 }
