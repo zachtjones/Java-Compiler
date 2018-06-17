364f98b330ca211c47ef2c7d0554b95ea3c6c87b
Zach Jones <zachtjones16@gmail.com>
2018-06-17 16:53:50 -0400

    Added type checking

--- intermediate/GetArrayValueStatement.java
+++ intermediate/GetArrayValueStatement.java
@@ -1,5 +1,10 @@
 package intermediate;
 
+import java.util.HashMap;
+
+import helper.CompileException;
+import helper.UsageCheck;
+
 public class GetArrayValueStatement implements InterStatement {
 	
 	Register array, index, result;
@@ -15,5 +20,20 @@ public class GetArrayValueStatement implements InterStatement {
 
 	public String toString() {
 		return "getArrayValue " + result + " = " + array + " @ " + index + ";";
+	}
+
+	@Override
+	public void typeCheck(HashMap<Register, String> regs) throws CompileException {
+		UsageCheck.verifyDefined(array, regs);
+		UsageCheck.verifyDefined(index, regs);
+		
+		// make sure the type of the array ends in []
+		if (!array.typeFull.endsWith("[]")) {
+			throw new CompileException("can't index type: " + array.typeFull);
+		}
+
+		result.typeFull = array.typeFull.substring(0, array.typeFull.length() - 2);
+
+		regs.put(result, result.typeFull);
 	}
 }
