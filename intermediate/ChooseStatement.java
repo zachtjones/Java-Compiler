85bfd83def38090f63062dcc350be859020aa478
Zach Jones <zachtjones16@gmail.com>
2018-06-17 16:53:39 -0400

    Added basic type checking - but not done

--- intermediate/ChooseStatement.java
+++ intermediate/ChooseStatement.java
@@ -1,5 +1,9 @@
 package intermediate;
 
+import java.util.HashMap;
+
+import helper.CompileException;
+
 /**
  * Represents a SSA choose between statement.
  * This statement is used to keep the resulting IL in 
@@ -18,5 +22,21 @@ public class ChooseStatement implements InterStatement {
 	@Override
 	public String toString() {
 		return result + " = choose(" + src1 + ", " + src2 + ");";
+	}
+
+	@Override
+	public void typeCheck(HashMap<Register, String> regs) throws CompileException {
+		// TODO left and right have to have common superclass, and the result
+		//   is that common superclass
+		// primitive types should already be done
+		// remember the boxing / un-boxing operations too
+		if (!src1.isPrimitive() || !src2.isPrimitive()) {
+			System.out.println("Choose statements type checking not complete for reference types");
+		}
+		if ( ! src1.typeFull.equals(src2.typeFull)) {
+			throw new CompileException("in the choose statement, the types were different. Add code to see if they are convertible.");
+		}
+		result.typeFull = src1.typeFull;
+		regs.put(result, result.typeFull);
 	}
 }
