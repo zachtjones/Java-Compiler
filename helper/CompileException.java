f22ec6d674e50b0eecfdee6dbede721262234c4c
Zach Jones <zachtjones16@gmail.com>
2018-09-02 21:38:11 -0700

    Added constructor with throwable cause

--- helper/CompileException.java
+++ helper/CompileException.java
@@ -1,7 +1,14 @@
 package helper;
 
 public class CompileException extends Exception {
+	
+	/** Constructs a new exception with the specified detail message */
 	public CompileException(String message) {
 		super(message);
+	}
+
+	/** Constructs a new exception with the specified detail message and cause. */
+	public CompileException(String string, Throwable cause) {
+		super(string, cause);
 	}
 }
