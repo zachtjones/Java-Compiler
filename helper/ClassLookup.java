ac412f94e71ceef7a98dd27649b0983c275f6910
Zach Jones <zachtjones16@gmail.com>
2018-09-23 13:04:38 -0700

    Uses file system for java. imports

--- helper/ClassLookup.java
+++ helper/ClassLookup.java
@@ -2,22 +2,15 @@ package helper;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-
+import main.JavaCompiler;
 import tree.ImportNode;
 import tree.NameNode;
 import tree.SymbolTable;
 
 public class ClassLookup {
 	private HashMap<String, String> items = new HashMap<String, String>();
-
-	private SymbolTable syms;
 	
 	/**
 	 * Loads the imports to reference the proper items.
@@ -29,10 +22,8 @@ public class ClassLookup {
 	 * @throws CompileException If there is an issue resolving creating this object.
 	 */
 	public ClassLookup(String fileName, String packageName, ArrayList<ImportNode> imports,
-			SymbolTable classLevel) throws CompileException {
-		
-		syms = classLevel;
-		
+			SymbolTable classLevel)	throws CompileException {
+
 		File thisFile = new File(fileName);
 		// automatically import the files in the same directory / package
 		// if default package (null), fully qualified == short name
@@ -55,41 +46,38 @@ public class ClassLookup {
 		javaLang.isAll = true;
 		javaLang.name = new NameNode(fileName, -1);
 		javaLang.name.primaryName = "java.lang";
-		
-		// add in the java files
-		try {
-			File f = new File("javaLookup.txt");
-			// use the javadocs online to lookup the list and create it
-			if (!f.exists()) {
-				URL url = new URL("https://docs.oracle.com/javase/8/docs/api/allclasses-noframe.html");
-				Scanner sc = new Scanner(url.openStream());
-				PrintWriter pw = new PrintWriter("javaLookup.txt");
-				while (sc.hasNextLine()) {
-					String line = sc.nextLine();
-					if (line.matches("<li><a href=\".*")) {
-						String name = line.replaceAll("<li><a href=\"", "");
-						name = name.replaceAll("\\.html.*", "");
-						pw.println(name.replace('.', '/'));
-					}
-				}
-				sc.close();
-				pw.flush();
-				pw.close();
-			}
-			// read from the list
-			List<String> lines = Files.readAllLines(f.toPath());
-			for (String line : lines) {
-				// just the last part of the fully qualified name
-				String shortestName = line.replaceAll(".*/", "");
-				items.put(shortestName, line);
-				items.put(line, line);
-				items.put(line.replace('/', '.'), line);
-				syms.putEntry(line, "className", fileName, -1);
-			}
-		} catch(IOException e) {
-			throw new CompileException(e.getMessage(), fileName, -1);
-		}
+		imports.add(javaLang);
 
+		for (ImportNode i : imports) {
+			String name = i.name.primaryName;
+			if (name.startsWith("java.")) {
+				if (i.isAll) {
+					File folderName = new File(JavaCompiler.javaDir + "/" + name.replace('.', '/'));
+					String[] files = folderName.list();
+					for (String file : files) {
+						if (file.endsWith(".java")) {
+							String shortName = file.replace(".java", "");
+							String fullName = name.replace('.', '/') + '/' + shortName;
+							items.put(shortName, fullName);
+							items.put(fullName.replace('/', '.'), fullName);
+							items.put(fullName, fullName);
+						}
+					}
+				} else {
+					String simpleName = name.substring(name.lastIndexOf('.') + 1);
+					String fullyQualified = name.replace('.', '/');
+					items.put(simpleName, fullyQualified);
+					items.put(fullyQualified, fullyQualified);
+					items.put(name, fullyQualified);
+				}
+			}
+		}
+		// put all the class names in the symbol table
+		for (String i : items.values()) {
+			// can't add it twice
+			if (classLevel.lookupThisScope(i) == -1)
+				classLevel.putEntry(i, "class", fileName, -1);
+		}
 	}
 
 	/** Gets the full name from the short name representation.
@@ -101,14 +89,14 @@ public class ClassLookup {
 	 * */
 	public String getFullName(String shortName, String fileName, int line)
 			throws CompileException {
-		
+
 		// handle Object -> java/lang/Object, 
 		//   java.lang.Object -> java/lang/Object, 
 		//   java/lang/Object -> java/lang/Object
 		if (items.containsKey(shortName)) {
 			return items.get(shortName).replace('.', '/');
 		}
-		
+
 		// handle System.in -> java/lang/System.in
 		if (shortName.contains(".")) {
 			String firstPart = shortName.substring(0, shortName.indexOf("."));
@@ -117,7 +105,7 @@ public class ClassLookup {
 				return items.get(firstPart) + "." + rest;
 			}
 		}
-		
+
 		// return the short name, assume default package;
 		return shortName;
 	}
