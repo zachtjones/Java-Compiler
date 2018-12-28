188687fd6124e8c09152b34f14012400f15276b2
Zach Jones <zachtjones16@gmail.com>
2018-12-28 12:11:48 -0500

    Implemented GetParamStatement -> x64

--- src/main/java/intermediate/GetParamStatement.java
+++ src/main/java/intermediate/GetParamStatement.java
@@ -3,18 +3,25 @@ package intermediate;
 import java.util.HashMap;
 
 import helper.CompileException;
+import x64.X64File;
+import x64.X64Function;
+import x64.instructions.MoveInstruction;
+import x64.operands.X64NativeRegister;
+import x64.operands.X64PreservedRegister;
 
 /** getParam %register = name */
 public class GetParamStatement implements InterStatement {
 	Register r;
-	String localName;
+	private String localName;
 	
 	private final String fileName;
 	private final int line;
+
+	private InterFunction func;
 	
 	/**
 	 * Creates a new get parameter variable statement.
-	 * @param registerNum The register number to set
+	 * @param r The register number to set
 	 * @param localName The parameter to get.
 	 */
 	public GetParamStatement(Register r, String localName, String fileName, int line) {
@@ -32,11 +39,37 @@ public class GetParamStatement implements InterStatement {
 	@Override
 	public void typeCheck(HashMap<Register, String> regs, HashMap<String, String> locals,
 			HashMap<String, String> params, InterFunction func) throws CompileException {
+
+		this.func = func;
 		
 		if (!params.containsKey(localName)) {
 			throw new CompileException("'" + localName + "' not a parameter.", fileName, line);
 		}
 		
 		regs.put(r, params.get(localName));
+	}
+
+	@Override
+	public void compile(X64File assemblyFile, X64Function function) {
+
+		// the args are as follows: JNI, obj/class, actual args
+		if (localName.equals("this")) {
+			function.addInstruction(
+				new MoveInstruction(
+					X64NativeRegister.RSI,
+					X64PreservedRegister.fromILRegister(r)
+				)
+			);
+		} else {
+			// localName is guaranteed to be in the list, as type checking was performed
+			int paramIndex = 3 + func.paramNames.indexOf(localName);
+
+			function.addInstruction(
+				new MoveInstruction(
+					X64NativeRegister.argNumbered(paramIndex),
+					X64PreservedRegister.fromILRegister(r)
+				)
+			);
+		}
 	}
 }
--- src/main/java/x64/operands/X64NativeRegister.java
+++ src/main/java/x64/operands/X64NativeRegister.java
@@ -2,42 +2,51 @@ package x64.operands;
 
 import x64.Instruction;
 
-import static x64.Instruction.Size.QUAD;
-
 /**
  * This class represents the addressable registers for the native x64 register set.
  */
-public class X64NativeRegister extends X64PreservedRegister {
+public class X64NativeRegister implements SourceOperand, DestinationOperand {
 
 	/** Represents the first argument register */
-	public static final X64NativeRegister RDI = new X64NativeRegister(QUAD, "%rdi");
+	public static final X64NativeRegister RDI = new X64NativeRegister("%rdi");
 	/** Represents the second argument register */
-	public static final X64NativeRegister RSI = new X64NativeRegister(QUAD, "%rsi");
+	public static final X64NativeRegister RSI = new X64NativeRegister("%rsi");
 	/** Represents the third argument register */
-	public static final X64NativeRegister RDX = new X64NativeRegister(QUAD, "%rdx");
+	public static final X64NativeRegister RDX = new X64NativeRegister("%rdx");
 	/** Represents the fourth argument register */
-	public static final X64NativeRegister RCX = new X64NativeRegister(QUAD, "%rcx");
+	public static final X64NativeRegister RCX = new X64NativeRegister("%rcx");
 	/** Represents the fifth argument register */
-	public static final X64NativeRegister R8 = new X64NativeRegister(QUAD, "%r8");
+	public static final X64NativeRegister R8 = new X64NativeRegister("%r8");
 	/** Represents the sixth argument register */
-	public static final X64NativeRegister R9 = new X64NativeRegister(QUAD, "%r9");
+	public static final X64NativeRegister R9 = new X64NativeRegister("%r9");
 
 	/** Represents the return register */
-	public static final X64NativeRegister RAX = new X64NativeRegister(QUAD, "%rax");
+	public static final X64NativeRegister RAX = new X64NativeRegister("%rax");
 
-	public static final X64NativeRegister RBX = new X64NativeRegister(QUAD, "%rbx");
-	public static final X64NativeRegister RBP = new X64NativeRegister(QUAD, "%rbp");
-	public static final X64NativeRegister R12 = new X64NativeRegister(QUAD, "%r12");
-	public static final X64NativeRegister R13 = new X64NativeRegister(QUAD, "%r13");
-	public static final X64NativeRegister R14 = new X64NativeRegister(QUAD, "%r14");
-	public static final X64NativeRegister R15 = new X64NativeRegister(QUAD, "%r15");
+	public static final X64NativeRegister RBX = new X64NativeRegister("%rbx");
+	public static final X64NativeRegister RBP = new X64NativeRegister("%rbp");
+	public static final X64NativeRegister R12 = new X64NativeRegister("%r12");
+	public static final X64NativeRegister R13 = new X64NativeRegister("%r13");
+	public static final X64NativeRegister R14 = new X64NativeRegister("%r14");
+	public static final X64NativeRegister R15 = new X64NativeRegister("%r15");
+
+	private static final X64NativeRegister[] args = { RDI, RSI, RDX, RCX, R8, R9 };
 
 	/** How this x64 machine register is represented in the assembly language*/
 	private final String representation;
 
-	private X64NativeRegister(Instruction.Size size, String representation) {
-		super(-1, size);
+	private X64NativeRegister(String representation) {
 		this.representation = representation;
+	}
+
+	/** Returns the argument numbered, starting from 1 as %rdi */
+	public static X64NativeRegister argNumbered(int paramIndex) {
+		return args[paramIndex - 1];
+	}
+
+	@Override
+	public Instruction.Size getSuffix() {
+		return Instruction.Size.QUAD;
 	}
 
 	@Override
