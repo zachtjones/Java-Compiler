0cba03b347c034f8dce37834c40c318e72783b90
Zach Jones <zachtjones16@gmail.com>
2019-01-29 15:59:41 -0500

    Fixed bug with mac os

--- src/main/java/x64/instructions/CallClassMethod.java
+++ src/main/java/x64/instructions/CallClassMethod.java
@@ -3,6 +3,8 @@ package x64.instructions;
 import x64.Instruction;
 import x64.SymbolNames;
 
+import static x64.allocation.CallingConvention.isLinux;
+
 /**
  * Represents a call to a routine specified by the class/method names.
  * These calls should be put in the procedure linkage table
@@ -12,7 +14,7 @@ public class CallClassMethod implements Instruction {
     private final String label;
 
     public CallClassMethod(String className, String name) {
-        label = SymbolNames.getMethodName(className, name) + "@PLT";
+        label = SymbolNames.getMethodName(className, name) + (isLinux ? "@PLT" : "");
     }
 
     public CallClassMethod(String label) {
