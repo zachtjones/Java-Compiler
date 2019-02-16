fb1519f64b9ca49271cbfec8da8aa5b207fbb69a
Zach Jones <zachtjones16@gmail.com>
2019-02-15 21:26:21 -0500

    Allocates with a priority based on usage count

--- src/main/java/x64/allocation/RegisterTransformer.java
+++ src/main/java/x64/allocation/RegisterTransformer.java
@@ -1,14 +1,14 @@
 package x64.allocation;
 
+import helper.MapUtils;
+import org.jetbrains.annotations.NotNull;
 import x64.Instruction;
 import x64.X64Context;
 import x64.instructions.*;
 import x64.operands.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-import static x64.allocation.CallingConvention.argumentRegister;
 import static x64.allocation.CallingConvention.preservedRegistersNotRBP;
 import static x64.operands.X64NativeRegister.RBP;
 import static x64.operands.X64NativeRegister.RSP;
@@ -70,7 +70,7 @@ public class RegisterTransformer {
 		Deque<RegisterMapped> temporaryStack = new ArrayDeque<>();
 		int maxTemp = 0;
 
-		Map<X64PreservedRegister, RegisterMapped> mapping = new HashMap<>();
+		HashMap<X64PreservedRegister, RegisterMapped> mapping = new HashMap<>();
 
 		for (int i = 0; i < initialContents.size(); i++) {
 			// we can use a register on both operands of the instruction
@@ -113,15 +113,20 @@ public class RegisterTransformer {
 			}
 		}
 
+		// calculate if there's enough hardware registers, if there is, the problem is simple:
+		//  allocate a register that can be temporary to a temp one if possible, otherwise use a preserved one
+		//  allocate a preserved register to a hardware preserved register
 		if (hasEnoughHardwareRegs(maxPreserved, maxTemp)) {
 
+			// obtain the mapping
 			Map<X64PreservedRegister, X64NativeRegister> nativeMapping = getNatives(maxTemp, mapping);
 
-
+			// obtain the new list of instructions, with them swapped out
 			for (Instruction i : initialContents) {
 				i.allocateRegisters(nativeMapping);
 			}
 
+			// determine the function prologue and epilogue
 			AllocationUnit au = new AllocationUnit();
 			X64RegisterOperand[] preserved = CallingConvention.preservedRegisters();
 
@@ -143,7 +148,8 @@ public class RegisterTransformer {
 			return au;
 		} else {
 
-			RegisterMapping registerMapping = new RegisterMapping(maxTemp - 1, mapping);
+			// transforms the instructions out, replacing 2 memory op ones with an intermediate calculation
+			int spaceNeeded = rbpTransform(initialContents, mapping, tempsAvailable);
 
 			// we know we use all the registers
 			AllocationUnit au = new AllocationUnit();
@@ -158,7 +164,7 @@ public class RegisterTransformer {
 			au.prologue.add(new MoveInstruction(RSP, RBP));
 
 			// spaceNeeded should be oddNumber * 8.
-			//au.prologue.add(new SubtractInstruction(new Immediate(registerMapping.spaceNeeded), RSP));
+			au.prologue.add(new SubtractInstruction(new Immediate(spaceNeeded), RSP));
 
 			au.epilogue.addFirst(new PopInstruction(RBP));
 			au.epilogue.addFirst(new MoveInstruction(RBP, RSP));
@@ -176,29 +182,15 @@ public class RegisterTransformer {
 
 			throw new RuntimeException(mapping.toString());
 		}
-
-		// iterate through the instructions, converting the X64PreservedRegister's to the X64NativeRegister's
-//		for (Instruction i : initialContents) {
-//			i.allocateRegisters(mapping);
-//		}
-//
-//		Set<X64NativeRegister> usedPreservedRegs = new HashSet<>();
-//		for (X64NativeRegister reg : mapping.values()) {
-//			if (reg != X64NativeRegister.R10.nativeOne && reg != X64NativeRegister.R11.nativeOne) {
-//				usedPreservedRegs.add(reg);
-//			}
-//		}
-//
-//		// add another one if there are an even number used -- might add one already in the set
-//		// since we just need to preserve one, do one of the arguments
-//		while (usedPreservedRegs.size() % 2 == 0) {
-//			usedPreservedRegs.add(argumentRegister(1).nativeOne);
-//		}
-
-//		return usedPreservedRegs;
 	}
 
-
+	/**
+	 * Performs the calculation that there are enough hardware registers for the allocation.
+	 * @param numPreserved The number of preserved registers required.
+	 * @param numTemporary The number of temporary registers required.
+	 * @return A boolean that is if there are enough hardware registers.
+	 * Otherwise, the base pointer offsets will be used to store spilled registers
+	 */
 	private boolean hasEnoughHardwareRegs(int numPreserved, int numTemporary) {
 
 		X64RegisterOperand[] preservedPossible = CallingConvention.preservedRegisters();
@@ -212,6 +204,13 @@ public class RegisterTransformer {
 			totalNumAvailable >= numPreserved + numTemporary;
 	}
 
+	/**
+	 * Obtains a mapping of the pseudo registers to hardware registers from the mapping with registerMapped.
+	 * @param numTemporary The number of temporary registers in the registerMapped map.
+	 *                     If there are more than temps available on the list, preserved registers will be mapped to.
+	 * @param mapping The mapping of pseudo registers to their register numbers if there is plenty of both types.
+	 * @return The valid mapping of pseudo registers to their hardware ones.
+	 */
 	private Map<X64PreservedRegister, X64NativeRegister> getNatives(int numTemporary, Map<X64PreservedRegister, RegisterMapped> mapping) {
 
 		X64RegisterOperand[] preservedPossible = CallingConvention.preservedRegisters();
@@ -240,43 +239,69 @@ public class RegisterTransformer {
 		return nativeMap;
 	}
 
-	public class RegisterMapping {
+	/**
+	 * transforms the instructions RegisterMapped to native registers and base-pointer offsets.
+	 * @param mapping The mapping of allocation if there were infinite registers,
+	 *                each being used as many times as possible.
+	 * @param tempsAvailable The hardware registers available as temporaries (may contain unused argument registers)
+	 * @return The number of bytes of stack space that needs allocated.
+	 * This amount will maintain stack alignment.
+	 */
+	private int rbpTransform(@NotNull HashMap<X64PreservedRegister, RegisterMapped> mapping,
+							 @NotNull List<X64NativeRegister> tempsAvailable) {
 
-		/** This is the space needed on the stack. Note that this will be (odd number * 8) bytes */
-		public final long spaceNeeded;
+		// todo, loop through counting jumps to increase priority level,
+		//  jumps backwards = 3x as much, conditional jumps backwards 2x as much
+		// Could also do some prediction based on other heuristics, like forward jumps as well
 
-		RegisterMapping(int numTemporariesRequired, Map<X64PreservedRegister, RegisterMapped> mapping) {
-
-			// todo, loop through counting jumps to increase priority level,
-			//  jumps backwards = 3x as much, conditional jumps backwards 2x as much
-			// Could also do some prediction based on other heuristics, like forward jumps as well
-
-			// right now, a simple priority incremented by the number of usages
-			for (Instruction i : initialContents) {
-				 i.prioritizeRegisters(mapping);
-			}
-
-			// now we can determine the which registers are more important, lower important ones get mapped
-			//  to base-pointer offsets
-			TreeSet<RegisterMapped> priorities = new TreeSet<>(mapping.values());
-
-			ArrayList<X64NativeRegister> tempsLeft = new ArrayList<>(tempsAvailable);
-
-			// this particular register is available for transitions of memory to memory to 2 instructions
-			X64NativeRegister temporaryIntermediate = tempsLeft.remove(0);
-
-			ArrayList<X64NativeRegister> preservedLeft = new ArrayList<>(Arrays.asList(preservedRegistersNotRBP()));
-
-			// allocate them, pulling from the lists until they're empty, then start allocating stack space
-			HashMap<RegisterMapped, X64NativeRegister> nativeAllocations = new HashMap<>();
-			HashMap<RegisterMapped, BasePointerOffset> basePointerOffsets = new HashMap<>();
-			
-			// TODO
-			System.out.println("Priorities: " + priorities);
-			System.out.println("Temp used as intermediate: " + temporaryIntermediate);
-			System.out.println("Temps available list: " + tempsLeft);
-			System.out.println("Preserved available list: " + preservedLeft);
+		// right now, a simple priority incremented by the number of usages
+		for (Instruction i : initialContents) {
+			i.prioritizeRegisters(mapping);
 		}
+
+		// now we can determine the which registers are more important, lower important ones get mapped
+		//  to base-pointer offsets
+		TreeSet<RegisterMapped> priorities = new TreeSet<>(mapping.values());
+
+		LinkedList<X64NativeRegister> tempsLeft = new LinkedList<>(tempsAvailable);
+
+		// this particular register is available for transitions of memory to memory to 2 instructions
+		final X64NativeRegister temporaryIntermediate = tempsLeft.remove(0);
+
+		LinkedList<X64NativeRegister> preservedLeft = new LinkedList<>(Arrays.asList(preservedRegistersNotRBP()));
+
+		// allocate them, pulling from the lists until they're empty, then start allocating stack space
+		HashMap<RegisterMapped, X64NativeRegister> nativeAllocations = new HashMap<>();
+		HashMap<RegisterMapped, BasePointerOffset> basePointerOffsets = new HashMap<>();
+
+		RegisterMapped next;
+		int stackNumberAllocated = 0;
+		while ((next = priorities.pollFirst()) != null) { // this will exhaust priorities queue
+
+			if (!next.needsPreserved && tempsLeft.size() > 0) { // can allocate as temp register
+				nativeAllocations.put(next, tempsLeft.removeFirst());
+
+			} else if (preservedLeft.size() > 0) { // can allocate as preserved register
+				nativeAllocations.put(next, preservedLeft.removeFirst());
+
+			} else { // ran out, start allocating stack space, first one -8(%rbp)
+				stackNumberAllocated++;
+				basePointerOffsets.put(next, new BasePointerOffset(8 * -stackNumberAllocated));
+			}
+		}
+
+		if (stackNumberAllocated % 2 == 0) {
+			stackNumberAllocated++;
+		}
+
+		HashMap<X64PreservedRegister, X64NativeRegister> natives = MapUtils.map(mapping, nativeAllocations);
+		HashMap<X64PreservedRegister, BasePointerOffset> locals = MapUtils.map(mapping, basePointerOffsets);
+
+		// TODO transform the instructions, replacing all the pseudo registers out.
+		//  Some will require using a temporary registers as an intermediate one when the
+		//    instruction would have 2 memory operands
+
+		return stackNumberAllocated * 8;
 	}
 }
 
