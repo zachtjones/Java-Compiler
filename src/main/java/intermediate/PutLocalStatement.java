bc037734080d4529551c229483633caba8692968
Zach Jones <zachtjones16@gmail.com>
2019-02-27 14:34:00 -0500

    Performs a conversion when necessary when you assign to local variables

--- src/main/java/intermediate/PutLocalStatement.java
+++ src/main/java/intermediate/PutLocalStatement.java
@@ -7,8 +7,10 @@ import helper.TypeChecker;
 import helper.Types;
 import helper.UsageCheck;
 import org.jetbrains.annotations.NotNull;
+import x64.Boxing;
 import x64.X64Context;
 import x64.operands.X64PseudoRegister;
+import x64.pseudo.ConvertPseudoInstruction;
 import x64.pseudo.MovePseudoToPseudo;
 
 /** PutLocal name = %register */
@@ -18,6 +20,8 @@ public class PutLocalStatement implements InterStatement {
 	
 	@NotNull private final String fileName;
 	private final int line;
+
+	private Types localType;
 	
 	/**
 	 * Creates a new put local variable statement.
@@ -45,16 +49,40 @@ public class PutLocalStatement implements InterStatement {
 			throw new CompileException("local variable: " + localName + " is not defined.",
 					fileName, line);
 		}
-		TypeChecker.canDirectlyAssign(locals.get(localName), r.getType(), fileName, line);
+		localType = locals.get(localName);
+		TypeChecker.assertCanAssign(locals.get(localName), r.getType(), fileName, line);
 	}
 
 	@Override
 	public void compile(@NotNull X64Context context) throws CompileException {
+
+		// TODO everywhere canDirectlyAssign is invoked, can do an extra instruction that converts between types.
+
 		final X64PseudoRegister destination = context.getLocalVariable(localName);
 
-		// copy the result over to the destination
-		context.addInstruction(
-			new MovePseudoToPseudo(r.toX64(), destination)
-		);
+		final Types sourceType = r.getType();
+
+		if (TypeChecker.isDirectlyAssignable(sourceType, localType)) {
+			// simple copy the result over to the destination
+			context.addInstruction(
+				new MovePseudoToPseudo(r.toX64(), destination)
+			);
+		} else if (TypeChecker.isCastAssignable(sourceType, localType)) {
+			// convert source to middle
+			// move middle to destination
+			X64PseudoRegister temp = context.getNextRegister(localType);
+			context.addInstruction(
+				ConvertPseudoInstruction.from(r.toX64(), temp)
+			);
+			context.addInstruction(
+				new MovePseudoToPseudo(temp, destination)
+			);
+
+		} else if (TypeChecker.isBoxingAssignable(sourceType, localType)) {
+			// box/unbox operation
+			Boxing.insertNecessaryCode(r.toX64(), destination);
+		}
+
+
 	}
 }
