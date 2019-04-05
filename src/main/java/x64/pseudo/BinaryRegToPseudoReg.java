19cac8543a883c48596b8821f732eb8f2b3c2f55
Zach Jones <zachtjones16@gmail.com>
2019-04-04 20:31:43 -0400

    Refactored binary op register, pseudoRegister

--- src/main/java/x64/pseudo/BinaryRegToPseudoReg.java
+++ src/main/java/x64/pseudo/BinaryRegToPseudoReg.java
@@ -1,9 +1,15 @@
 package x64.pseudo;
 
 import org.jetbrains.annotations.NotNull;
+import x64.allocation.AllocationContext;
 import x64.allocation.RegistersUsed;
+import x64.instructions.*;
+import x64.operands.BPOffset;
 import x64.operands.X64PseudoRegister;
 import x64.operands.X64Register;
+
+import java.util.Collections;
+import java.util.List;
 
 /***
  * Represents a binary instruction that involves a pseudo register source and native register destination.
@@ -16,8 +22,7 @@ public abstract class BinaryRegToPseudoReg implements PseudoInstruction {
 	private final String name;
 
 
-	public BinaryRegToPseudoReg(String name, @NotNull X64Register source,
-								@NotNull X64PseudoRegister destination) {
+	BinaryRegToPseudoReg(String name, @NotNull X64Register source, @NotNull X64PseudoRegister destination) {
 		this.name = name;
 		this.source = source;
 		this.destination = destination;
@@ -26,6 +31,31 @@ public abstract class BinaryRegToPseudoReg implements PseudoInstruction {
 	@Override
 	public void markRegisters(int i, RegistersUsed usedRegs) {
 		usedRegs.markDefined(destination, i);
+	}
+
+	@NotNull
+	abstract BinaryRegToReg createThisRegToReg(@NotNull X64Register source, @NotNull X64Register destination);
+
+	@NotNull
+	abstract BinaryRegToBPOffset createThisRegToBPOffset(@NotNull X64Register source, @NotNull BPOffset destination);
+
+	@Override
+	public final @NotNull List<@NotNull Instruction> allocate(@NotNull AllocationContext context) {
+		if (context.isRegister(destination)) {
+			return Collections.singletonList(
+				createThisRegToReg(
+					source,
+					context.getRegister(destination)
+				)
+			);
+		} else {
+			return Collections.singletonList(
+				createThisRegToBPOffset(
+					source,
+					context.getBasePointer(destination)
+				)
+			);
+		}
 	}
 
 	/** Represents how this instruction should be represented */
--- src/main/java/x64/pseudo/MoveRegToPseudo.java
+++ src/main/java/x64/pseudo/MoveRegToPseudo.java
@@ -1,15 +1,13 @@
 package x64.pseudo;
 
 import org.jetbrains.annotations.NotNull;
-import x64.allocation.AllocationContext;
-import x64.instructions.Instruction;
+import x64.instructions.BinaryRegToBPOffset;
+import x64.instructions.BinaryRegToReg;
 import x64.instructions.MoveRegToBPOffset;
 import x64.instructions.MoveRegToReg;
+import x64.operands.BPOffset;
 import x64.operands.X64PseudoRegister;
 import x64.operands.X64Register;
-
-import java.util.Collections;
-import java.util.List;
 
 public class MoveRegToPseudo extends BinaryRegToPseudoReg {
 
@@ -19,23 +17,12 @@ public class MoveRegToPseudo extends BinaryRegToPseudoReg {
 	}
 
 	@Override
-	public @NotNull List<@NotNull Instruction> allocate(@NotNull AllocationContext context) {
-		if (context.isRegister(destination)) {
-			return Collections.singletonList(
-				new MoveRegToReg(
-					source,
-					context.getRegister(destination),
-					destination.getSuffix()
-				)
-			);
-		} else {
-			return Collections.singletonList(
-				new MoveRegToBPOffset(
-					source,
-					context.getBasePointer(destination),
-					destination.getSuffix()
-				)
-			);
-		}
+	@NotNull BinaryRegToReg createThisRegToReg(@NotNull X64Register source, @NotNull X64Register destination) {
+		return new MoveRegToReg(source, destination, this.destination.getSuffix());
+	}
+
+	@Override
+	@NotNull BinaryRegToBPOffset createThisRegToBPOffset(@NotNull X64Register source, @NotNull BPOffset destination) {
+		return new MoveRegToBPOffset(source, destination, this.destination.getSuffix());
 	}
 }
