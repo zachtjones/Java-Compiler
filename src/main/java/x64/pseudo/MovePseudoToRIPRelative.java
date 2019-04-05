e2ff452533e28e45f89fcafdbd37a6649abfe6a3
Zach Jones <zachtjones16@gmail.com>
2019-04-04 20:26:06 -0400

    Refactored binary op pseudoReg, displacement(%rip)

--- src/main/java/x64/pseudo/MovePseudoToRIPRelative.java
+++ src/main/java/x64/pseudo/MovePseudoToRIPRelative.java
@@ -1,16 +1,11 @@
 package x64.pseudo;
 
 import org.jetbrains.annotations.NotNull;
-import x64.allocation.AllocationContext;
-import x64.instructions.Instruction;
-import x64.instructions.MoveBPOffsetToReg;
+import x64.instructions.BinaryRegToRIPRelative;
 import x64.instructions.MoveRegToRIPRelative;
 import x64.operands.RIPRelativeData;
 import x64.operands.X64PseudoRegister;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import x64.operands.X64Register;
 
 public class MovePseudoToRIPRelative extends BinaryPseudoToRIPRelative {
 
@@ -19,30 +14,10 @@ public class MovePseudoToRIPRelative extends BinaryPseudoToRIPRelative {
 		super("mov", source, destination);
 	}
 
+
 	@Override
-	public @NotNull List<@NotNull Instruction> allocate(@NotNull AllocationContext context) {
-		if (context.isRegister(source)) {
-			return Collections.singletonList(
-				new MoveRegToRIPRelative(
-					context.getRegister(source),
-					destination,
-					source.getSuffix()
-				)
-			);
-		} else {
-			// need temp, can't have 2 memory operands in 1 instruction
-			return Arrays.asList(
-				new MoveBPOffsetToReg(
-					context.getBasePointer(source),
-					context.getScratchRegister(),
-					source.getSuffix()
-				),
-				new MoveRegToRIPRelative(
-					context.getScratchRegister(),
-					destination,
-					source.getSuffix()
-				)
-			);
-		}
+	@NotNull BinaryRegToRIPRelative createThisRegToRipRelative(@NotNull X64Register source,
+															   @NotNull RIPRelativeData destination) {
+		return new MoveRegToRIPRelative(source, destination, this.source.getSuffix());
 	}
 }
