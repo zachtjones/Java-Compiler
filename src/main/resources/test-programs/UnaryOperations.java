e2cee6fac75427f997e90ba1e2d0534df89d3850
Zach Jones <zachtjones16@gmail.com>
2019-05-24 22:55:21 -0400

    Added test program for unary operations

---
+++ src/main/resources/test-programs/UnaryOperations.java
@@ -0,0 +1,13 @@
+
+public class UnaryOperations {
+    public static void main(String[] args) {
+        System.out.println(-128);
+        System.out.println(!true);
+        System.out.println(Integer.toBinaryString(~0b0101_0000_1111_0000_1101_0101_1111_0000));
+
+        // Total printout
+        // -128
+        // false
+        // 10101111000011110010101000001111
+    }
+}
